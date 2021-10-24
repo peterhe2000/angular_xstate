82a0c1a950fdbdc5903b6bbca63436e1a1e5299e
Peter He <peter@mathspathway.com>
2021-10-24 12:33:33 +1100

    Add ngrx-store-localstorage to sync redux store to localstorage

--- ss-frontend/ss/apps/app1/src/app/app-store.module.ts
+++ ss-frontend/ss/apps/app1/src/app/app-store.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import {
+  ActionReducer,
+  MetaReducer,
+  StoreModule
+} from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { NxModule } from '@nrwl/angular';
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['user'],
+    rehydrate: true,
+  })(reducer);
+}
+
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 @NgModule({
   imports: [
     StoreModule.forRoot(
       {},
       {
-        metaReducers: !environment.production ? [] : [],
+        metaReducers: !environment.production ? metaReducers : [],
         runtimeChecks: {
           strictStateImmutability: true,
           strictActionImmutability: true,
